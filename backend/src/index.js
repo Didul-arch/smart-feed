3f40dd260c75041a2d42b5a828c82bd1895be18f
tokyoboi0711 <azmididul@gmail.com>
2025-06-01 10:53:58 +0700

    sisa deploy dah ini ngurusin cors doang sumpah

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,24 +1,21 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const dotenv = require('dotenv');
-const createRoute = require('./routes/index');
-// const { startScheduler } = require('./scheduler');
+const dotenv = require("dotenv");
+const createRoute = require("./routes/index");
 dotenv.config();
 
 const PORT = process.env.PORT;
 
-app.get('/healthcheck', (req, res) => {
-    res.json({
-        message: 'Halalaloaoo! Page utama',
-        uptime: process.uptime(),
-    });
+app.get("/healthcheck", (req, res) => {
+  res.json({
+    message: "Halalaloaoo! Page utama",
+    uptime: process.uptime(),
+  });
 });
 
+// HAPUS function createRoute ini, udah ada di routes/index.js
 createRoute(app);
 
-// startScheduler();
-
-//anuan
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
